ce5fb0f95e2731c279bab197a364ad9892329c11
electrolyte-orb <55880176+electrolyte-orb@users.noreply.github.com>
2023-05-24 10:22:32 +0530

    refresh session if session expired

--- next-server/middleware.ts
+++ next-server/middleware.ts
@@ -8,7 +8,9 @@ export async function middleware(req: NextRequest) {
 	const res = NextResponse.next();
 	const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
 	try {
-		await supabase.auth.getSession();
+		await supabase.auth.getSession().catch(async () => {
+			await supabase.auth.refreshSession();
+		});
 	} catch (err) {}
 	return res;
 }
